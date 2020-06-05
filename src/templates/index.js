e7a98fc7e0eb852dfdf73afcde4ef3f658870f88
Oren Farhi <farhioren@gmail.com>
2020-06-05 13:34:03 -0400

    update(theme): dark bg colors

--- src/templates/index.js
+++ src/templates/index.js
@@ -21,7 +21,7 @@ class BlogIndex extends React.Component {
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="All posts" />
         <Bio />
-        <Logger content={data} />
+        <Logger content={{ data, pageContext }} />
         <Search />
         <Pagination
           {...pageContext}
