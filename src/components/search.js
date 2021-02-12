7b9ca3810332b95c9b9300c3125c4ccff1a96f8b
Oren Farhi <farhioren@gmail.com>
2021-02-12 11:57:13 -0500

    add(search): algolia

--- src/components/search.js
+++ src/components/search.js
@@ -1,13 +1,42 @@
 import React from "react"
 import { ExternalLink } from "../components/external-link"
 import algoliasearch from "algoliasearch/lite"
-import { InstantSearch, SearchBox, Hits } from "react-instantsearch-dom"
+import {
+  InstantSearch,
+  SearchBox,
+  Snippet,
+  Hits,
+} from "react-instantsearch-dom"
+const filterResult =
+  "Front End Engineer, React Consultant, Software Architect, Javascript, Angular and much more"
+const Hit = ({ hit }) =>
+  hit.description === filterResult ? null : (
+    <a href={hit.url} className="m-auto ">
+      <article className="has-background-white-bis has-text-centered mt-3 is-flex-column section">
+        <h2 className="space-bottom-3">
+          <a href={hit.url} className="m-auto ">
+            {hit.title}
+          </a>
+          <div class="media section is-flex-column">
+            <figure className="image is-128x128 media-left">
+              <img src={hit.image} />
+            </figure>
+            <Snippet
+              hit={hit}
+              attribute="description"
+              className="subtitle media-content"
+            />
+          </div>
+        </h2>
+      </article>
+    </a>
+  )
 
 const searchClient = algoliasearch(
   "OHCG2E3NIE",
   "1d6a1b6eac52dac38869d0f5a6ac930f"
 )
-
+const orizensIndex = "netlify_9242e31d-4a5c-41c7-8c4d-86a46f4b0c49_master_all"
 export default function Search({ section = "blog" }) {
   // const [query, setQuery] = React.useState(null)
 
@@ -16,18 +45,27 @@ export default function Search({ section = "blog" }) {
   //   setQuery(ev.target.value)
   // }
   return (
-    <div className="is-flex is-justify-center">
-      <ExternalLink
-        href={`www.google.com/search?q=site:orizens.com/${section}`}
-        target="_blank"
-      >
-        search orizens.com/{section} with Google
-      </ExternalLink>
-      {/* <InstantSearch indexName="orizens" searchClient={searchClient}>
-        <SearchBox />
-        <Hits />
-      </InstantSearch> */}
-    </div>
+    <>
+      <div className="is-flex-column is-justify-center">
+        {/* <ExternalLink
+          href={`www.google.com/search?q=site:orizens.com/${section}`}
+          target="_blank"
+        >
+          search orizens.com/{section} with Google
+        </ExternalLink> */}
+        <InstantSearch indexName={orizensIndex} searchClient={searchClient}>
+          <SearchBox className="is-justify-center is-flex" />
+          <div class="panel">
+            <Snippet
+              attribute="description"
+              hit={Hit}
+              // Optional parameters
+            />
+          </div>
+          <Hits hitComponent={Hit} />
+        </InstantSearch>
+      </div>
+    </>
     // <form onSubmit={handleSearch}>
     //   <div className="field">
     //     <div className="control">
