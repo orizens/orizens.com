23c09b9166c9e6ceab7fedc4534c9e65bf03e642
Oren Farhi <farhioren@gmail.com>
2023-12-12 11:12:42 -0500

    chore: cleanup

--- src/components/icon.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-export function Icon({ name, prefix = "las", className = "" }) {
-  return <i className={`${prefix} la-${name} mr-1 ${className}`} />
-}
--- src/components/logger.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export const Logger = ({ content }) => {
-  return (
-    process.env.NODE_ENV !== `production` && (
-      <button onClick={() => console.log(content)}>log</button>
-    )
-  )
-}
